bf9906ed35e9adac1a131dc5d60d0ba211bf3487
SDev <Devime7289128912@outlook.com>
2026-09-21 19:51:46 +0200

    Bump service worker shell cache to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "voidforge-shell-v7";
+const CACHE_NAME = "voidforge-shell-v8";
 const CACHE_PREFIX = "voidforge-shell-";
 const OFFLINE_URL = new URL("./index.html", self.registration.scope).href;
 
